54a8ea2e799afae5af730173a4df27b9635b226e
kabinja <renaudrwemalika@gmail.com>
2021-04-30 09:11:46 +0200

    update running scripts

--- run-all-arja-local.sh
+++ run-all-arja-local.sh
@@ -5,16 +5,14 @@ run()
 	PROJECT=$1
 	BUG=$2
 
-	for FLAKE_RATE in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+	for FLAKE_RATE in 0.00 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10
 	do
-		for FLAKE_STRATEGY in vocabulary uniformDistribution
+		for FLAKE_STRATEGY in vocabulary
 		do
 			./run-arja-local.sh ${PROJECT} ${BUG} ${FLAKE_RATE} ${FLAKE_STRATEGY} 
 		done
 	done
 }
-
-run jsoup 1
 
 run lang 20 
 run lang 22
@@ -28,3 +26,4 @@ run math 53
 run math 58
 run math 70
 run math 98
+
