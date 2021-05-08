1a39380e9849c3676ce39e20c3b3562bbf6aec07
kabinja <renaudrwemalika@gmail.com>
2021-05-08 18:41:07 +0200

    test run

--- run-sbfl-all.sh
+++ run-sbfl-all.sh
@@ -11,7 +11,7 @@ run()
 	PROJECT=$1
 	BUG=$2
 
-	for FLAKE_RATE in 0.00 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10
+	for FLAKE_RATE in 0.00 #0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10
 	do
     	for FLAKE_STRATEGY in vocabulary
 		do
@@ -22,7 +22,7 @@ run()
                 export PATH=$HOME/bin/apache-maven-3.6.1/bin:$PATH
 
                 sbatch --output=sbfl-${PROJECT}-${BUG}-${FLAKE_RATE}-${FLAKE_STRATEGY}.out \
-                --time=0-2:00:00
+                --time=0-2:00:00 \
                 ./run-sbfl.sh ${PROJECT} ${BUG} ${FLAKE_RATE} ${FLAKE_STRATEGY} 
             else
                 ./run-sbfl.sh ${PROJECT} ${BUG} ${FLAKE_RATE} ${FLAKE_STRATEGY}
@@ -31,11 +31,11 @@ run()
 	done
 }
 
-run lang 20 
-run lang 22
-run lang 39
+#run lang 20 
+#run lang 22
+#run lang 39
 
-#run math 5
+run math 5
 #run math 22
 #run math 39
 #run math 50
